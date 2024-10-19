e5dcf941ec39b00e2b53d929407c78a909a4e1eb
Brad Cocar <brad.cocar@gmail.com>
2024-10-18 17:14:34 -0700

    Validated remove and view buttons to require a current selection. Added save button functionality.

--- src/main/java/com/example/filmtracker/Main.java
+++ src/main/java/com/example/filmtracker/Main.java
@@ -6,11 +6,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-
-import java.util.ArrayList;
 
 
 public class Main extends Application {
